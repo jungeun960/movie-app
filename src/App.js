456501e3c56cf3cbe09a351c2123adebde8dcd37
Ubuntu <ubuntu@ip-172-31-43-212.ap-northeast-2.compute.internal>
2020-07-13 05:19:48 +0000

    Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import PropType from "prop-types";
 
 class App extends React.Component{
   state ={
-    count:0
+    isLoading : true
   };
-  add = () => {
-    this.setState(current => ({count : current.count +1 }));
-  };
-  minus = () => {
-    this.setState(current => ({count : current.count -1 }));
-  };
+  componentDidMount() { // 페이지 생성 시 가장 먼저 동작
+    setTimeout(() => {
+      this.setState({isLoading : false });
+    }, 6000); // 6초뒤에 동작한다.
+  }
 
   render() {
+    const {isLoading} = this.state;
     return (
       <div>
-        <h1> The number is :  {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? "Loading" : "We are ready"}
         </div>
   )
   }
