b4c1ee7cb20eb0d77fc821d545d41ab0e75e5605
Ubuntu <ubuntu@ip-172-31-43-212.ap-northeast-2.compute.internal>
2020-07-13 06:37:52 +0000

    Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state ={
-    isLoading : true
+    isLoading : true,
+    movies : []
   };
+  getMovies = async () => { // axios가 데이터를 가져올때까지 기다린다.
+    // async가 있어야 await 사용 가능
+    const {
+      data: {
+        data: {movies}
+      }
+    }= await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading : false}); // state에 movies값 넣기
+    console.log(movies);
+  };
+
   componentDidMount() { // 페이지 생성 시 가장 먼저 동작
-    setTimeout(() => {
-      this.setState({isLoading : false });
-    }, 6000); // 6초뒤에 동작한다.
+    this.getMovies();
   }
 
   render() {
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
     return (
       <div>
-        {isLoading ? "Loading" : "We are ready"}
+        {isLoading ? "Loading"
+              : movies.map(movie => (
+              <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+            />
+  ))}
         </div>
   )
   }
