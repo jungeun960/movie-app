5a8766453fb8605e4e690576f7d1dafddbbd95f5
Ubuntu <ubuntu@ip-172-31-43-212.ap-northeast-2.compute.internal>
2020-07-15 17:16:47 +0000

    Updates

---
+++ precache-manifest.5f74face625f0535320e5435baebda9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab78691809e751173a4c9c604773e13e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "400817c793aa9205be4c",
+    "url": "/static/css/main.44154c37.chunk.css"
+  },
+  {
+    "revision": "d9cd6dfec6bca1f1795d",
+    "url": "/static/js/2.d710c1aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d710c1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400817c793aa9205be4c",
+    "url": "/static/js/main.ab0e5134.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  }
+]);
